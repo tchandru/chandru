0dcc816009996c86b82c3a6cf344d53d7e402198
tchandru <35715653+tchandru@users.noreply.github.com>
2018-01-26 08:52:52 +0000

    Create pser.c

---
+++ pser.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ float x,y,z,c;
+ printf("\nEnter your hours",x);
+ scanf("%f",&x);
+ printf("\nEnter your minutes",y);
+ scanf("%f",&y);
+ printf("\nEnter your seconds",z);
+ scanf("%f",&z);
+ c=x*60+y;
+ printf("%f",c);
+}
+
