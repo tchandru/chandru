71fc5b3b28af660b610e71a3dfa3aefc819f89b5
tchandru <35715653+tchandru@users.noreply.github.com>
2018-02-04 12:20:43 +0530

    Create largest three number.c

---
+++ largest three number.c
@@ -0,0 +1,18 @@
+
+21 lines (13 sloc) 241 Bytes
+#include<stdio.h>
+int main(){
+{
+int p,q,r;
+printf("Enter Three Number\n");
+scanf("%d%d%d",&p,&q,&r);
+
+if(p>q&&p>r)
+    printf("%d is Grater",p);
+else if(b>c)
+    printf("%d id Greater",q);
+else
+    printf("%d is Greater",r);
+}
+
+
