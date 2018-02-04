55c1c7028ce99e4452c8b53c0ccd08995bf925fd
tchandru <35715653+tchandru@users.noreply.github.com>
2018-02-04 12:07:06 +0530

    Create Alphabet.c

---
+++ Alphabet.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char a;
+    printf("Enter an alphabet: ");
+    scanf("%c",&a);
+        printf("%c is a vowel.", a);
+    else
+        printf("%c is a consonant.", a);
+    return 0;
+}
+
