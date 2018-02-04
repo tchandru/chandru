7932ad6cb7cd6a21e1d04b2982636815f46fb15b
tchandru <35715653+tchandru@users.noreply.github.com>
2018-02-04 16:44:47 +0530

    Create TTT.c

---
+++ TTT.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+   int a,b,c,d,e;
+ 
+   printf("enter the first item price : \t",a);
+   scanf("%d",&a);
+   printf("enter the second item price : \t",b);
+   scanf("%d",&b);
+         c=a+b;
+   printf("total price :  %d   \t",c);
+  printf("\nenter the amount given by student costomer  : \t",d);
+  scanf("%d",&d);
+  printf("\nenter the amount returned by shop keeper  : \t",e);
+  scanf("%d",&e);
+}
