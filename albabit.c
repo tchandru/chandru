0838d2412870b2e6398e2695a7a723b124d0d0ee
tchandru <35715653+tchandru@users.noreply.github.com>
2018-02-04 12:11:08 +0530

    Create albabit.c

---
+++ albabit.c
@@ -0,0 +1,16 @@
+es
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the letter");
+scanf("%d",&ch);
+if(ch>=a)&&(ch<=z)
+{
+printf("%c is alphat" ch);
+}
+else
+{
+printf("%c is not alphat" ch);
+}
+}
