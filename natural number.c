1ac5c101bb6949b0ea19bff6d334713146e64302
tchandru <35715653+tchandru@users.noreply.github.com>
2018-02-06 16:40:09 +0530

    Create natural number.c

---
+++ natural number.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+void main()
+{
+    int m,x=0;
+    printf("Enter the natural numbers  : ");
+    scanf("%d",&m);
+for(int i=0;i<=m;i++)
+    
+        x=x+i;
+    
+    printf("%d",x);
+}
