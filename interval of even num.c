d73089f8a1c17d23cfccf1753199c23a7eb6bf3e
tchandru <35715653+tchandru@users.noreply.github.com>
2018-02-09 16:53:47 +0530

    Create interval of even num.c

---
+++ interval of even num.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+void main()
+{
+
+int x,y,i=0;
+ printf("enter the  starting number    : ",x);
+ scanf("%d",&x);
+ printf("enter the ending number    :  ",y);
+ scanf("%d",&y);
+for(i=x;i<=y;i++)
+{
+if(i%2!=0);
+else
+printf("%d\n",i);
+}
+    
+}
