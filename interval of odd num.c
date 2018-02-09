ebdb642c2aca54d2c1457d7f64135d91772c9037
tchandru <35715653+tchandru@users.noreply.github.com>
2018-02-09 16:34:26 +0530

    Create interval of odd num.c

---
+++ interval of odd num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+
+int a,b,i=0;
+ printf("enter the  starting number    : ",a);
+ scanf("%d",&a);
+ printf("enter the ending number    :  ",b);
+ scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+if(i%2==0);
+else
+printf("%d\n",i);
+}
+    
+}
